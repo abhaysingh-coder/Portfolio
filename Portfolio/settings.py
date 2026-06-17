2238b0fdd05096b05f5eb90c5ec2377b77f4a24d
abhaysingh-coder <srinetabhaysingh14@gmail.com>
2026-06-18 01:53:12 +0530

    updated database settings

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        default=f"sqlite:///{str(BASE_DIR / 'db.sqlite3')}",
+        conn_max_age=600,
     )
 }
 
